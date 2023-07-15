ff055bf1e3cd17ea356f75228eb390ecfeadb2c1
nowfuture011130 <105948127+nowfuture011130@users.noreply.github.com>
2023-07-15 16:11:59 -0400

    update

---
+++ src/modules/Orders/index.js
@@ -0,0 +1,47 @@
+import orders from "../../../src/dashboard/orders.json";
+import { Card, Table, Tag } from "antd";
+
+const Orders = () => {
+  const renderorderStatus = (status) => {
+    if (status === "Accepted") {
+      return <Tag color="green">{status}</Tag>;
+    }
+    if (status === "Pending") {
+      return <Tag color="orange">{status}</Tag>;
+    }
+    if (status === "Declined") {
+      return <Tag color="red">{status}</Tag>;
+    }
+  };
+  const tableColumns = [
+    {
+      title: "Order ID",
+      dataIndex: "orderID",
+      key: "orderID",
+    },
+    {
+      title: "Delivery Address",
+      dataIndex: "deliveryAddress",
+      key: "deliveryAddress",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+      render: (price) => `${price} $`,
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
+      render: renderorderStatus,
+    },
+  ];
+  return (
+    <Card title={"Orders"} style={{ margin: 20 }}>
+      <Table dataSource={orders} columns={tableColumns} rowKey="orderID" />
+    </Card>
+  );
+};
+
+export default Orders;
